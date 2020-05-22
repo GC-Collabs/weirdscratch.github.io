c63cad4b7af9ad0e0c2d60b7078c041ec63e6395
Durvenson <dray@oogzy.com>
2020-05-22 13:59:43 -0400

    MAJOR changes
    combining elements ftw!

--- elementalthing/elemental.js
+++ elementalthing/elemental.js
@@ -1,9 +1,10 @@
 function combinationof() {
 	var elemental = event.srcElement.id;
 	combo = combo.concat([' ' + elemental]);
+	comboshow = comboshow.concat([' ' + elemresult2(elemental,1)]);
 	document.getElementById("combo").style.width = Math.pow(combo.length,0.5)*100;
 	document.getElementById("combo").style.height = Math.pow(combo.length,0.5)*100;
-	document.getElementById("combo").innerHTML = combo.toString();
+	document.getElementById("combo").innerHTML = comboshow.toString();
 	document.getElementById("combo").style.backgroundColor = "#44FF44";
 }
 function getRGBValues(str) {
@@ -14,22 +15,38 @@ function getRGBValues(str) {
 		'b': vals[2]
 	};
 }
-function createButton(elementthing, color) {
+function createButton(elementthing, elementthing2, color) {
+	if (document.getElementById(elementthing2) == null) {
 	var button = document.createElement("button");
 	button.innerHTML = elementthing;
-	button.id = elementthing;
+	button.id = elementthing2;
 	button.style.backgroundColor = color;
+	button.style.marginTop = "3px";
+	button.style.marginBottom = "3px";
+	button.style.marginLeft = "3px";
+	button.style.marginRight = "3px";
 	var rgb = color.match(/\d+/g);
 	if (rgb[0] + rgb[1] + rgb[2] < 383) {
 		button.style.color = "#FFFFFF";
 	}
 	button.setAttribute( "onClick", "combinationof()");
 	document.getElementById("elements").appendChild(button);
+	}
 }
-function elemresult(combinations,id) {
+function elemresult(array) {
+	createButton(elemresult2(array,0),elemresult3(array,0)[1], elemresult3(array,0)[2]);
+}
+function elemresult2(combinations,id) {
 	for (var i = 0; i < Object.values(json).length; i++) {
-		if (Object.values(json)[i][id] == combinations) {
-			return i;
+		if (Object.values(json)[i][id].toString()==combinations.toString()) {
+			return Object.keys(json)[i];
+		}
+	}
+}
+function elemresult3(combinations,id) {
+	for (var i = 0; i < Object.values(json).length; i++) {
+		if (Object.values(json)[i][id].toString()==combinations.toString()) {
+			return Object.values(json)[i];
 		}
 	}
 }
@@ -42,6 +59,7 @@ document.addEventListener('keyup', function (event) {
 
 	if (key === 'Backspace') {
 		combo.length = combo.length - 1;
+		comboshow.length = comboshow.length - 1;
 		if (combo.length == 0) {
 			document.getElementById("combo").innerHTML = "Click on elements to add them to the combo list.";
 			document.getElementById("combo").style.width = 200;
@@ -51,11 +69,12 @@ document.addEventListener('keyup', function (event) {
 		else {
 			document.getElementById("combo").style.width = Math.pow(combo.length,0.5)*100;
 			document.getElementById("combo").style.height = Math.pow(combo.length,0.5)*100;
-			document.getElementById("combo").innerHTML = combo.toString();
+			document.getElementById("combo").innerHTML = comboshow.toString();
 			document.getElementById("combo").style.backgroundColor = "#44FF44";
 		}
 	}
 })
 
 let combo = [];
-var json = {"Earth": [[],1,"#BB6644"], "Fire": [[],2,"#FF8844"], "Air": [[],3,"#8888FF"], "Water": [[],4,"#4444FF"], "Dust": [[1,3],5,"#888888"]}
+let comboshow = [];
+var json = {"Earth": [[1],"1","#BB6644"], "Fire": [[1],"2","#FF8844"], "Air": [[1],"3","#8888FF"], "Water": [[1],"4","#4444FF"], "Dust": [[" 1"," 3"],"5","#888888"]}
